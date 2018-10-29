b8bb8411839ec6451874c34601e257152073bb10
Leesssssssss <772592971@qq.com>
2018-10-29 20:16:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const oneStarting = require('./one/oneStarting');
+const twoStarting = require('./two/twoStarting');
+const threeStarting = require('./three/threeStarting');
+const fourStarting = require('./four/fourStarting');
+const express = require('express')
+
+threeStarting('A03170092','3184669mm');
